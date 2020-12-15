2a68d09105609a017fc8c7fcb8bd8359cc39865c
RihabKouira <rihabkouira1@gmail.com>
2020-12-15 16:23:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+class App extends React.Component {
+  state = {
+    person: {
+      fullName: "Alicia Keys",
+      bio:
+        "Born in 1981, in New York, Alicia Keys began piano lessons at age 7",
+      imgPub: "/Alicia.jpg",
+      profession: "Singer-songwriter",
+    },
+    visibility: false,
+    counter: 0,
+  };
+  handleClick = () => {
+    this.setState({ visibility: !this.state.visibility });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ counter: this.state.counter + 1 });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.visibility ? (
+          <header className="App-header">
+            <h3>Full Name : {this.state.person.fullName}</h3>
+            <h3> Biography : {this.state.person.bio}</h3>
+            <h3> Profession : {this.state.person.profession}</h3>
+
+            <img src={this.state.person.imgPub} alt="Alicia" width="400px" />
+          </header>
+        ) : (
+          <h1> Click the Button to see the Info</h1>
+        )}
+        <button onClick={this.handleClick}>click Me !</button>
+        <h2> {this.state.counter}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
